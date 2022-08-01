49bae55e7f537eb778a5f5a520a4827504325cb1
JessicaSaantos <jessicaferre998@gmail.com>
2022-08-01 00:20:23 -0300

    terminando regra de negocio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Base
 const inputTempo = document.querySelector("#tempoInput")
 const button = document.querySelector(".simular")
+let pontosAcumulados = 0
 
 // Funcionalidade que mostra o tempo
 function mostrarTempo (){
@@ -14,7 +15,36 @@ inputTempo.addEventListener("change" , mostrarTempo)
 
 // Funcionalidade que calcula os pontos
 function calcularPontos (){
+
     const inputValor = document.getElementById("valorInput").value  
     const pontosDia = document.getElementById("pontosDiarios")
-    const pontosMeses = document.getElementById("pontosMensais") 
+    const pontosMes = document.getElementById("pontosMensais") 
+
+    const meses = inputTempo.value
+
+    if (meses == 0){
+        pontosMes.innerText = 0
+
+        if (inputValor < 40 ) {
+            pontosDia.innerText = 0
+        } else {
+            let resultado = inputValor / 40
+            pontosDia.innerText = resultado
+        }
+    } 
+    else {
+        if (inputValor < 40 ) {
+            pontosDia.innerText = 0
+            pontosMes.innerText = 0
+        } else {
+            let resultado = inputValor / 40
+            pontosDia.innerText = resultado
+
+            let totalMensal = resultado * 30 
+            pontosAcumulados = pontosAcumulados + totalMensal
+            pontosMes.innerText = pontosAcumulados
+        }
+    }
 }
+
+button.addEventListener("click" , calcularPontos)
