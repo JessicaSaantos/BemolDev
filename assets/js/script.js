872cfe116d2d474903627f13995c5d5182c64b6e
JessicaSaantos <jessicaferre998@gmail.com>
2022-07-31 23:51:18 -0300

    adicionando funcionalidades

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 // Base
 const inputTempo = document.querySelector("#tempoInput")
-
+const button = document.querySelector(".simular")
 
 // Funcionalidade que mostra o tempo
 function mostrarTempo (){
+
+    // pesquisar sobre label
     let labelTempo = document.querySelector("#tempoLabel")
+    labelTempo.innerText = `${inputTempo.value} meses`
 }
-// 
+
+inputTempo.addEventListener("change" , mostrarTempo)
+
+// Funcionalidade que calcula os pontos
+function calcularPontos (){
+    
+}
