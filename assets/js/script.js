4e5b5d79fe96d608f6173765d126d884fd01aa66
JessicaSaantos <jessicaferre998@gmail.com>
2022-07-31 23:34:18 -0300

    iniciando JS

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Base
+const inputTempo = document.querySelector("#tempoInput")
+
+
+// Funcionalidade que mostra o tempo
+function mostrarTempo (){
+    let labelTempo = document.querySelector("#tempoLabel")
+}
+// 
